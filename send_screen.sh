8720efa3a24cb1f2db1d522d2a7537301952b5f0
maxg <maxg@negativrekord.de>
2016-12-31 20:24:18 +0100

    x.

--- send_screen.sh
+++ send_screen.sh
@@ -1,2 +1,5 @@
-vlc screen:// :screen-fps=18 :live-caching=5 :sout='#transcode{vcodec=mp4v,keyint=9,vb=1000,width=800,height=600}:rtp{dst=192.168.0.3,port=1234,sdp=rtsp://192.168.0.2:8080/test.sdp}' 
-#vlc simpsons.mp4 :sout='#transcode{vcodec=mp4v,width=480,height=320}:rtp{dst=192.168.0.3,port=1234,sdp=rtsp://192.168.0.2:8080/test.sdp}' 
+#vlc -vvv screen:// :screen-fps=18 :live-caching=5 :sout='#transcode{vcodec=mp4v,keyint=9,vb=1000,width=800,height=600}:rtp{dst=192.168.1.80,port=1234,sdp=rtsp://192.168.1.77:8080/test.sdp}' 
+
+#vlc -vvv "screen:// :screen-fps=25 :live-caching=5"  :sout='#transcode{vcodec=mp4v,vb=4000,fps=25,keyint=25,acodec=none}:rtp{dst=192.168.1.80,port=1234,sdp=rtsp://192.168.1.77:8080/test.sdp}' 
+
+vlc -vvv screen:// --screen-fps=15 --live-caching=400  :sout='#transcode{vcodec=mp4v,vb=4000,fps=25,keyint=25,acodec=none}:rtp{dst=192.168.1.80,port=1234,sdp=rtsp://192.168.1.77:8080/test.sdp}' 
--- send_transcode.sh
+++ send_transcode.sh
@@ -1,1 +1,2 @@
-vlc "simpsons.mp4" :sout='#transcode{vcodec=h264,vb=350,fps=24,keyint=24,acodec=none}:rtp{dst=192.168.0.3,port=1234,sdp=rtsp://192.168.0.2:8080/test.sdp}' 
+#vlc -vvv "bbb.mp4"  :sout='#transcode{vcodec=h264,vb=350,fps=24,keyint=24,acodec=none}:rtp{dst=192.168.1.80,port=1234,sdp=rtsp://192.168.1.77:8080/test.sdp}' 
+vlc -vvv "bbb.mp4"  :sout='#transcode{vcodec=mp4v,vb=2000,fps=15,keyint=15,acodec=none}:rtp{dst=192.168.1.80,port=1234,sdp=rtsp://192.168.1.77:8080/test.sdp}' 
